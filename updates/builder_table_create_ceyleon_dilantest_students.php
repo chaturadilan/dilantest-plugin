8368b07934ddaa506b001f9b11b26002f384828d
Chatura Dilan <dilan@dilan.me>
2022-06-14 06:43:38 +0530

    Initial commit

---
+++ updates/builder_table_create_ceyleon_dilantest_students.php
@@ -0,0 +1,23 @@
+<?php namespace Ceyleon\Dilantest\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateCeyleonDilantestStudents extends Migration
+{
+    public function up()
+    {
+        Schema::create('ceyleon_dilantest_students', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->text('description');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('ceyleon_dilantest_students');
+    }
+}
